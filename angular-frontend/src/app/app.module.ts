e4001c12245e146d68d4522b78c8fde6904ced87
xixas <vikas.kyatannawar@gmail.com>
2024-05-15 15:59:43 +0530

    feat: demo project for microsoft sso

---
+++ angular-frontend/src/app/app.module.ts
@@ -0,0 +1,69 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import {
+  MsalModule,
+  MsalRedirectComponent,
+  MsalGuard,
+  MsalInterceptor,
+} from "@azure/msal-angular"; // Import MsalInterceptor
+import {
+  InteractionType,
+  PublicClientApplication,
+} from "@azure/msal-browser";
+
+const isIE =
+  window.navigator.userAgent.indexOf("MSIE ") > -1 ||
+  window.navigator.userAgent.indexOf("Trident/") > -1;
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MsalModule.forRoot(
+      new PublicClientApplication({
+        auth: {
+          clientId: "3cbc0352-015d-4af0-a774-0fe24ef29767",
+          authority:
+            "https://login.microsoftonline.com/common",
+          redirectUri: "http://localhost:4200",
+        },
+        cache: {
+          cacheLocation: "localStorage",
+          //storeAuthStateInCookie: isIE,
+        },
+      }),
+      {
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+          scopes: ["user.read"],
+        },
+      },
+      {
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map([
+          ["https://graph.microsoft.com/v1.0/me", ["user.read"]],
+        ]),
+      }
+    ),
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    MsalGuard,
+  ],
+  bootstrap: [AppComponent, MsalRedirectComponent],
+})
+export class AppModule {}
